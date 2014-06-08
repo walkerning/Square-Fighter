b34068e43fd105aacc5164683ec40027232c3230
Foxfi <foxdoraame@gmail.com>
2014-06-08 14:00:30 +0800

    调试qLearn 加入了训练模式功能，加入了从已有lib继续训练功能，修改了一个reverseState 的bug 把result显示改了一下...

--- qLearn.py
+++ qLearn.py
@@ -5,6 +5,7 @@ import AIagents
 import copy
 from loopLearn import printResult
 
+MAGIC_STRING = "LEARN PATTERN FILE\n"
 R = 0.9
 SQUARE_TOTAL = 89
 INI_WEIGHT = (1.0516153729117497, -1.7406298037151497, 2.2668134620369664, -1.705074288351434)
@@ -65,20 +66,18 @@ def Training(trainingDatas, RawStateLib, finalUtility):
             tmp = RawStateLib[newState][playerIndex]
             uPrimeList.append(tmp)
         #感觉需要两边对称更新才有用
-        if not uPrimeList:
-            uPrimeList = [0]
+        #if not uPrimeList:
+            #uPrimeList = [0]
         if datum not in  RawStateLib:
-            if max(uPrimeList):
-                print max(uPrimeList)
             #RawStateLib[datum][playerIndex] = immediatePayback(datum, playerIndex) + R * max(uPrimeList)
             #print "imm:", immediatePayback(datum, playerIndex)
             RawStateLib[datum] = [immediatePayback(datum, playerIndex) + R * max(uPrimeList), -immediatePayback(datum, playerIndex)-R * max(uPrimeList)]
+            #print "max uprime:", max(uPrimeList), "imm:", immediatePayback(datum, playerIndex) ,"new datum:",RawStateLib[datum]
             reDatum = datum.getReverseState()
             RawStateLib[reDatum] = [-R * max(uPrimeList) - immediatePayback(datum, playerIndex), immediatePayback(datum, playerIndex) + R * max(uPrimeList)]
         else:
-            #要不要考虑一下用和为0的权值
-            if max(uPrimeList):
-                print max(uPrimeList)
+            #if max(uPrimeList):
+            #print max(uPrimeList)
             #print "imm:", immediatePayback(datum, playerIndex)
             RawStateLib[datum][0] = immediatePayback(datum, playerIndex) + R * max(uPrimeList)#evalFunc(datum, 1, INI_WEIGHT)]
             RawStateLib[datum][1] = - RawStateLib[datum][0]
@@ -87,29 +86,103 @@ def Training(trainingDatas, RawStateLib, finalUtility):
             RawStateLib[reDatum][0] = - RawStateLib[reDatum][1]
 
 
-def Learning(times, learningAgent="qLearnAgent", start = StateLib(immediatePayback)):
+def Learning(times, learningAgent="qLearnAgent", learn_pattern = None, start_file = ''):
     if not hasattr(AIagents, learningAgent) or not hasattr(getattr(AIagents, learningAgent), 'getAction'):
         printAgentError(learningAgent)
         learningAgent = "qLearnAgent"
     agentClass = getattr(AIagents, learningAgent)
 
-    lib = start
-    agents = [agentClass(i, True) for i in range(2)]
+    # 导入已有数据
+    if not start_file:
+        lib = StateLib(immediatePayback)
+    else:
+        import cPickle
+        f = open(start_file)
+        try:
+            lib = cPickle.load(f)
+        except:
+            print "load base file fail, fall back to empty base lib"
+            pass
+        finally:
+            f.close()
+
+    learnPattern = []
+    # 分析训练模式文件
+    if learn_pattern:
+        f = open(learn_pattern)
+        magic_line = f.readline()
+        if magic_line != MAGIC_STRING:
+            print "训练模式文件必须以'LEARN PATTERN FILE'作为第一行"
+            exit(0)
+        import re
+        x1 = re.compile(r'\s+')
+        x2 = re.compile(r'\n')
+        for line in f.readlines():
+            line,_ = x2.subn('', line)
+            line = x1.split(line)
+            if '' in line:
+                line.remove('')
+            learnPattern.append(map(lambda x: eval(x), line))
+        curPattern = learnPattern.pop(0)
+        agents = [None, None]
+        if len(curPattern) == 3:
+            agents[curPattern[1]] = getattr(AIagents, learningAgent)(curPattern[1], True)
+            agents[1 - curPattern[1]] = getattr(AIagents, curPattern[2])(1 - curPattern[1])
+        elif len(curPattern) == 1:
+            agents = [agentClass(i, True) for i in range(2)]
+        else:
+            pass#wrong
+        print curPattern
+        f.close()
+    else:
+        agents = [agentClass(i, True) for i in range(2)]
+
 
     for i in range(times):
         trainingDatas = []
-        for agent in agents:
-            agent.setLib(lib)
+        if learn_pattern:
+            curPattern[0] -= 1
+            if curPattern[0] < 0:
+                if learnPattern:
+                    curPattern = learnPattern.pop(0)
+                    print curPattern
+                    if len(curPattern) == 3:
+                        agents[curPattern[1]] = getattr(AIagents, learningAgent)(curPattern[1], True)
+                        agents[curPattern[1]].setLib(lib)
+                        agents[1 - curPattern[1]] = getattr(AIagents, curPattern[2])(1 - curPattern[1])
+                    elif len(curPattern) == 1:
+                        agents = [agentClass(j, True) for j in range(2)]
+                        for agent in agents:
+                            agent.setLib(lib)
+                    else:
+                        pass#wrong
+                    curPattern[0] -= 1
+                else:
+                    learn_pattern = None
+                    agents = [agentClass(j ,True) for j in range(2)]
+                    for agent in agents:
+                        agent.setLib(lib)
+            else:
+                if len(curPattern) == 3:
+                    agents[curPattern[1]].setLib(lib)
+                elif len(curPattern) == 1:
+                    for agent in agents:
+                        agent.setLib(lib)
+                else:
+                    pass#wrong
+        else:
+            for agent in agents:
+                agent.setLib(lib)
         game = gameRunner.Game(agents, True)
         game.startGame()
         recordList = copy.deepcopy(game.recordList)
 
         printResult(recordList[-1], str(i))
-        import time, cPickle
-        filename = "qlearn--No.%d--"%i + time.strftime('%m-%d-%H-%M-%S',time.localtime(time.time())) + ".rep"
-        f = file(filename, 'w')
-        cPickle.dump(game.recordList, f)
-        f.close()
+        #import time, cPickle
+        #filename = "qlearn--No.%d--"%i + time.strftime('%m-%d-%H-%M-%S',time.localtime(time.time())) + ".rep"
+        #f = file(filename, 'w')
+        #cPickle.dump(game.recordList, f)
+        #f.close()
         if recordList[-1][1] == -1:
             utilityList = [TIE_UTILITY, TIE_UTILITY]
         elif recordList[-1][1] == 0:
@@ -126,10 +199,7 @@ def Learning(times, learningAgent="qLearnAgent", start = StateLib(immediatePayba
         trainingDatas.insert(0, (recordList[-1][-1], None))
         oldLib = copy.deepcopy(lib)
         Training(trainingDatas, lib, utilityList)
-        #print "oldLib", oldLib
-        #print "newLib", lib
-        #print "oldLib = lib?", oldLib == lib
-        #print "oldLIb.keys() = lib.keys()?", oldLib.keys() == lib.keys()
+
     import cPickle
     f = file(DATA_FILENAME, 'w')
     cPickle.dump(lib, f)
